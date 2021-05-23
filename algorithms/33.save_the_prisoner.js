b714f0c8cf31471bd9bef782971f6bc192a60093
Qahar Rahmani <qahar.shakib@gmail.com>
2021-05-23 22:09:36 +0430

    save the prisoner added

---
+++ algorithms/33.save_the_prisoner.js
@@ -0,0 +1,67 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'saveThePrisoner' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER m
+ *  3. INTEGER s
+ */
+
+function saveThePrisoner(n, m, s) {
+    // Write your code here
+    const result = (m%n) + s - 1;
+    if(result ===0){
+        return n;
+    }else if(result>n){
+        return result %n;
+    }else{
+        return result;
+    }
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const t = parseInt(readLine().trim(), 10);
+
+    for (let tItr = 0; tItr < t; tItr++) {
+        const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+        const n = parseInt(firstMultipleInput[0], 10);
+
+        const m = parseInt(firstMultipleInput[1], 10);
+
+        const s = parseInt(firstMultipleInput[2], 10);
+
+        const result = saveThePrisoner(n, m, s);
+
+        ws.write(result + '\n');
+    }
+
+    ws.end();
+}
