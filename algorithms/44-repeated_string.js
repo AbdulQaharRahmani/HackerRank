e0aa696b29ab142fea8bfab6dde70f99c687163b
Qahar Rahmani <qahar.shakib@gmail.com>
2021-05-28 20:48:29 +0430

    44 repeated string added

---
+++ algorithms/44-repeated_string.js
@@ -0,0 +1,66 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'repeatedString' function below.
+ *
+ * The function is expected to return a LONG_INTEGER.
+ * The function accepts following parameters:
+ *  1. STRING s
+ *  2. LONG_INTEGER n
+ */
+
+function repeatedString(s, n) {
+    // Write your code here
+    let fix = Math.floor(n / s.length);
+    let count = countA(s) * fix;
+    
+    let leftOut = n % s.length;
+    count = count + countA(s.substring(0,leftOut));
+    return count;
+    
+}
+function countA(s){
+    let count = 0;
+    for(let c of s){
+        if(c==='a'){
+            count++;
+        }
+    }
+    return count;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const s = readLine();
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const result = repeatedString(s, n);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
